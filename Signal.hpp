d7fd5d100d132f7b1b1bc22479d856c11ff4e89e
Beňuš <ondrej.benus@rechip.eu>
2020-11-18 19:56:29 +0100

    Optional multithread support

--- Signal.hpp
+++ Signal.hpp
@@ -9,30 +9,147 @@
  
 #pragma once
 #ifndef _ZEENO_SIGNAL_HPP
-#define _ZEENO_SIGNAL_HPP
+#	define _ZEENO_SIGNAL_HPP
 
-#include <algorithm>
-#include <functional>
-#include <memory>
-#include <vector>
+#	include <algorithm>
+#	include <functional>
+#	include <memory>
+#	include <mutex>
+#	include <vector>
 
 namespace Zeeno {
+
+struct dummy_mutex {
+	void lock() {
+	}
+	void unlock() {
+	}
+	bool try_lock() {
+		return true;
+	}
+};
 
 struct Connection {
 	virtual ~Connection()        = default;
 	virtual bool isValid() const = 0;
 	virtual bool disconnect()    = 0;
-	inline       operator bool() const {
-        return isValid();
+
+	inline operator bool() const {
+		return isValid();
 	}
 };
 
-template <typename... Arguments> class Signal {
+template<typename Mutex, typename... Arguments>
+class SignalWithMutex {
 public:
 	using Callback = std::function<void(Arguments...)>;
 
 	struct Connection final : Zeeno::Connection {
-		friend class Signal<Arguments...>;
+		friend class SignalWithMutex<Mutex, Arguments...>;
+
+		bool isValid() const override {
+			std::lock_guard<Mutex> lock{m_mutex};
+			return m_connected;
+		}
+		bool disconnect() override {
+			std::lock_guard<Mutex> lock{m_mutex};
+			return m_connected ? m_connected->disconnect(this) : false;
+		}
+
+	protected:
+		void setConnected(SignalWithMutex<Mutex, Arguments...>* sig) {
+			std::lock_guard<Mutex> lock{m_mutex};
+			m_connected = sig;
+		}
+
+	private:
+		SignalWithMutex<Mutex, Arguments...>* m_connected{nullptr};
+		Callback                              m_callback;
+		mutable Mutex                         m_mutex;
+	};
+
+	std::shared_ptr<Zeeno::Connection> connect(Callback f) {
+		std::shared_ptr<Connection> c = std::make_shared<Connection>();
+		c->m_callback                 = std::move(f);
+		c->setConnected(this);
+		{
+			std::lock_guard<Mutex> lock{m_mutex};
+			m_functions.push_back(c);
+		}
+		return std::move(c);
+	}
+
+	void operator()(Arguments... args) const {
+		if (isBlocked())
+			return;
+		decltype(m_functions) functions;
+		{
+			std::lock_guard<Mutex> lock{m_mutex};
+			functions = m_functions;
+		}
+		std::for_each(functions.cbegin(), functions.cend(), [&args...](const std::shared_ptr<Connection>& c) { c->m_callback(args...); });
+	}
+
+	inline void notify(Arguments... args) const {
+		operator()(args...);
+	}
+
+	inline bool disconnect(const std::shared_ptr<Connection>& c) {
+		std::lock_guard<Mutex> lock{m_mutex};
+		auto                   it = std::find(m_functions.cbegin(), m_functions.cend(), c);
+		if (it == m_functions.end())
+			return false;
+		(*it)->setConnected(nullptr);
+		m_functions.erase(it);
+		return true;
+	}
+
+	inline bool disconnect(const Connection* c) {
+		std::lock_guard<Mutex> lock{m_mutex};
+		auto                   it = std::find_if(m_functions.cbegin(), m_functions.cend(), [c](const std::shared_ptr<Connection>& s) { return s.get() == c; });
+		if (it == m_functions.end())
+			return false;
+		(*it)->setConnected(nullptr);
+		m_functions.erase(it);
+		return true;
+	}
+
+	inline void disconnectAll() {
+		std::lock_guard<Mutex> lock{m_mutex};
+		std::for_each(m_functions.begin(), m_functions.end(), [](const std::shared_ptr<Connection>& c) { c.setConnected(nullptr); });
+		m_functions.clear();
+	}
+
+	inline void blockSignal() {
+		m_block = true;
+	}
+
+	inline bool isBlocked() const {
+		return m_block;
+	}
+
+	inline void unblockSignal() {
+		m_block = false;
+	}
+
+	inline bool isConnected(const Connection& c) const {
+		std::lock_guard<Mutex> lock{m_mutex};
+		return std::find(m_functions.cbegin(), m_functions.cend(), c) != m_functions.cend();
+	}
+
+private:
+	std::vector<std::shared_ptr<Connection>> m_functions;
+	bool                                     m_block{false};
+	mutable Mutex                            m_mutex;
+};
+
+template<typename... Arguments>
+class SignalWithMutex<dummy_mutex, Arguments...> {
+public:
+	using Callback = std::function<void(Arguments...)>;
+
+	struct Connection final : Zeeno::Connection {
+		friend class SignalWithMutex<dummy_mutex, Arguments...>;
 
 		bool isValid() const override {
 			return m_connected;
@@ -41,24 +158,28 @@ public:
 			return m_connected ? m_connected->disconnect(this) : false;
 		}
 
+	protected:
+		void setConnected(SignalWithMutex<dummy_mutex, Arguments...>* sig) {
+			m_connected = sig;
+		}
+
 	private:
-		Signal<Arguments...>* m_connected{nullptr};
-		Callback              m_callback;
+		SignalWithMutex<dummy_mutex, Arguments...>* m_connected{nullptr};
+		Callback                                    m_callback;
 	};
 
 	std::shared_ptr<Zeeno::Connection> connect(Callback f) {
 		std::shared_ptr<Connection> c = std::make_shared<Connection>();
 		c->m_callback                 = std::move(f);
-		c->m_connected                = this;
+		c->setConnected(this);
 		m_functions.push_back(c);
 		return std::move(c);
 	}
 
 	void operator()(Arguments... args) const {
-		if (!isBlocked())
-			std::for_each(m_functions.begin(), m_functions.end(), [&args...](const std::shared_ptr<Connection>& c) {
-				c->m_callback(args...);
-			});
+		if (isBlocked())
+			return;
+		std::for_each(m_functions.cbegin(), m_functions.cend(), [&args...](const std::shared_ptr<Connection>& c) { c->m_callback(args...); });
 	}
 
 	inline void notify(Arguments... args) const {
@@ -69,7 +190,7 @@ public:
 		auto it = std::find(m_functions.cbegin(), m_functions.cend(), c);
 		if (it == m_functions.end())
 			return false;
-		(*it)->m_connected = nullptr;
+		(*it)->setConnected(nullptr);
 		m_functions.erase(it);
 		return true;
 	}
@@ -78,33 +199,39 @@ public:
 		auto it = std::find_if(m_functions.cbegin(), m_functions.cend(), [c](const std::shared_ptr<Connection>& s) { return s.get() == c; });
 		if (it == m_functions.end())
 			return false;
-		(*it)->m_connected = nullptr;
+		(*it)->setConnected(nullptr);
 		m_functions.erase(it);
 		return true;
 	}
 
 	inline void disconnectAll() {
-		std::for_each(m_functions.begin(), m_functions.end(), [](const std::shared_ptr<Connection>& c) { c.m_connected = nullptr; });
+		std::for_each(m_functions.begin(), m_functions.end(), [](const std::shared_ptr<Connection>& c) { c.setConnected(nullptr); });
 		m_functions.clear();
 	}
 
 	inline void blockSignal() {
 		m_block = true;
 	}
+
 	inline bool isBlocked() const {
 		return m_block;
 	}
+
 	inline void unblockSignal() {
 		m_block = false;
 	}
+
 	inline bool isConnected(const Connection& c) const {
 		return std::find(m_functions.cbegin(), m_functions.cend(), c) != m_functions.cend();
 	}
-	
+
 private:
 	std::vector<std::shared_ptr<Connection>> m_functions;
-	bool m_block{false};
+	bool                                     m_block{false};
 };
+
+template<typename... Args>
+using Signal = SignalWithMutex<dummy_mutex, Args...>;
 
 } // namespace Zeeno
 
